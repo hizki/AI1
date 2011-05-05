f02e39762fd3f2137564781c19b5c5d36e19c646
Guy Hizkiau <hizki@Hizkis-Macbook.local>
2011-05-05 14:09:50 +0300

    run_best_first added

---
+++ src/run_files/run_best_first.py
@@ -0,0 +1,53 @@
+# -*- coding:utf-8 -*-
+"""
+Created on May 4, 2011
+
+@author: inesmeya
+"""
+import run_me
+import heuristics
+from measure_core import TestAgent, ameasure
+import c_roomsets
+from anytime_best_first import AnytimeBestFirstGraphSearch
+import sys
+
+def best_first_depth(count,room_time_limit, seed):
+    ''' @param count: number of rooms
+    ''' 
+    #beam parametres:
+    depths = [10,80,250,400]
+    heuristic_s = [heuristics.PowerHeuristic2(),heuristics.LinearHeuristic()]
+        
+    #------------------ Create Agents ------------------            
+    agent_list = []
+    for h in heuristic_s:
+            for depth in depths: 
+                algorithm = AnytimeBestFirstGraphSearch(depth)
+                agent =     TestAgent(algorithm, h)
+                agent_list.append(agent)
+                
+    #---------------- Create Roomsets --------------------
+    roomsets = [c_roomsets.easy_roomset(count, seed),
+                c_roomsets.mild_roomset(count, seed),
+                c_roomsets.heavy_roomset(count, seed),
+                c_roomsets.static_rooms() ]
+    
+    dbs = ameasure(agent_list, roomsets, room_time_limit)
+    return dbs
+
+def main():
+    param = sys.argv[1]
+    cmain(param)
+
+ 
+def cmain(param):
+    mes_funs = [globals()[param]]
+    rooms_count= 100
+    room_limit = 50.0
+
+    it = rooms_count / 10
+    for i in range(it): 
+        run_me.run_tests(mes_funs, rooms_count, room_limit, i)
+
+if __name__ == "__main__":
+    main()
