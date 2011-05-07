b350a7c1f6e0968be0f14d2c969b8dc67d35e9e3
Guy Hizkiau <hizki@Hizkis-Macbook.local>
2011-05-07 16:49:45 +0300

    added new test to best_first

--- src/run_files/run_best_first.py
+++ src/run_files/run_best_first.py
@@ -10,6 +10,30 @@ from measure_core import TestAgent, ameasure
 import c_roomsets
 from anytime_best_first import AnytimeBestFirstGraphSearch
 import sys
+from search.utils import infinity
+
+def no_limit(count,room_time_limit, seed):
+    ''' @param count: number of rooms
+    ''' 
+    #best first parameters:
+    depths = [infinity]
+    heuristic_s = [heuristics.PowerHeuristic2(),heuristics.LinearHeuristic()]
+        
+    #------------------ Create Agents ------------------            
+    agent_list = []
+    for h in heuristic_s:
+            for depth in depths: 
+                algorithm = AnytimeBestFirstGraphSearch(depth)
+                agent =     TestAgent(algorithm, h)
+                agent_list.append(agent)
+                
+    #---------------- Create Roomsets --------------------
+    roomsets = [c_roomsets.easy_roomset(count, seed),
+                c_roomsets.mild_roomset(count, seed),
+                c_roomsets.heavy_roomset(count, seed) ]
+    
+    dbs = ameasure(agent_list, roomsets, room_time_limit)
+    return dbs
 
 def best_first_depth(count,room_time_limit, seed):
     ''' @param count: number of rooms
